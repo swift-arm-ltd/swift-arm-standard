bf1ed2fb8896b9adcdd40eccdbe5608e8d360558
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-16 10:00:31 +0200

    Add ARM standard smoke test (#7)

---
+++ Tests/ARM Standard Tests/CPU.ARM.Counter.Frequency Tests.swift
@@ -0,0 +1,24 @@
+// ===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-arm-standard open source project
+//
+// Copyright (c) 2024-2025 Coen ten Thije Boonkkamp and the swift-arm-standard project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// ===----------------------------------------------------------------------===//
+
+import Testing
+
+@testable import ARM_Standard
+
+extension CPU.ARM.Counter.Frequency {
+    @Suite struct Test {
+        @Test func `preserves its raw frequency`() {
+            let frequency = Self(24_000_000)
+
+            #expect(frequency.rawValue == 24_000_000)
+        }
+    }
+}
